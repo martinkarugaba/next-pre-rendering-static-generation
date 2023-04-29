5cacb4b45462e3879dd6693d6c7090f3c0c20192
Martin Karugaba <martinkarugaba21@gmail.com>
2023-04-29 13:31:05 +0300

    Add getStaticProps

---
+++ src/pages/users.js
@@ -0,0 +1,27 @@
+import User from "@/components/user";
+
+const UserList = ({users}) => {
+  return (
+    <>
+      <h1>List of users</h1>
+      {users.map((user) => (
+        <div key={user.id}>
+          <User user={user} />
+        </div>
+      ))}
+    </>
+  );
+};
+export default UserList;
+
+export const getStaticProps = async () => {
+  const url = "https://jsonplaceholder.typicode.com/users";
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return {
+    props: {
+      users: data,
+    },
+  };
+};
